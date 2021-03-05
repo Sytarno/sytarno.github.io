08a7af8e836dc8b847d46af06b56cb6e097ea10c
Evan Nguyen <evannguyen1101@yahoo.com>
2021-03-05 04:15:33 -0800

    main push

---
+++ sytarno-web/src/components/Bio/src-Bio.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./src-Bio.css";
+
+const Bio = () => {
+  return (
+    <section id="info-section">
+      <div id="namespace" className="text-center"></div>
+    </section>
+  );
+};
---
+++ sytarno-web/src/components/Landing/src-Landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./src-Landing.css";
+
+const Landing = () => {
+  return (
+    <section id="landing-section">
+      <div id="main-title" className="text-center">
+        <h1>Evan Nguyen</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
